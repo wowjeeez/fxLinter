a4b37774043b0329aa957f17f2507f3293152d4a
LedAndris <ledererandras2004@gmail.com>
2021-04-14 01:05:47 +0200

    feat(ruels/src): Updated rule to opinion only

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -7,5 +7,5 @@ export const en = {
     "no-get-player-ped-for-local": "Instead of GetPlayerPed(-1) and other obsolete solutions use PlayerPedId() to get the local ped, it's faster",
     "no-get-player-ped": "Are you sure that you need that GetPlayerPed there?",
     "use-fxmanifest": "Use fxmanifest.lua instead of __resource.lua",
-    "src": "Use local src = source instead of redefining source, it has better readability"
+    "src": "Use local src = source instead of _source, it has better readability"
 }
--- src/plugins.ts
+++ src/plugins.ts
@@ -60,7 +60,7 @@ rules.set("use-fxmanifest", (line: string, idx: number, file: string) => {
   return null
 })
 rules.set("src", (line: string, idx: number, file: string) => {
-  if (line.match(new RegExp("local\s*source\s*=\s*source"))) {
+  if (line.match(new RegExp("local\s*_source\s*=\s*source"))) {
     return {line: idx, file, rule: "src", lineTxt: line, level: "warn"}
   }
   return
